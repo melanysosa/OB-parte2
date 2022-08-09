1c40aa7e75c82564e6e6812a2df6ab55846824eb
melanysosa <sosamelanynoelia@gmail.com>
2022-08-09 20:09:52 -0300

    Numbers en JS

---
+++ Javascript/Ejercicio5-Numeros en js/ejercicio5.js
@@ -0,0 +1,7 @@
+const alturaCm = 154;
+const alturaM = 1.54;
+const pesokg = 60.1;
+const alturaRedondeada = Math.ceil(alturaM);
+const pesoRedondeado = Math.floor(pesokg);
+const max_value_js = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(max_value_js);
