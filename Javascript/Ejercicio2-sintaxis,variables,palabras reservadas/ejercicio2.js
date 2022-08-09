abefb6ee3201eb6aeb6b17f6b2bfb1d2491ebcb5
melanysosa <sosamelanynoelia@gmail.com>
2022-08-09 10:18:08 -0300

    sintaxis

---
+++ Javascript/Ejercicio2-sintaxis,variables,palabras reservadas/ejercicio2.js
@@ -0,0 +1,13 @@
+const lista = [
+	"Melany",
+	26,
+	true,
+	new Date(1995, 8, 25),
+	{
+		titulo: 'Orgullo y prejuicio',
+		autor: 'Jane Austea',
+		fecha: new Date(1813, 01, 28),
+		url: 'https://www.amazon.com/-/es/Jane-Austen/dp/1101969784',
+	},
+];
+console.log(lista)
